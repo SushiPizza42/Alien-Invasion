304358a5ded06c0b4ae249573d39298547904bdc
Alasdair <beenhed@hotmail.com>
2022-03-24 13:34:34 +0000

    Checking weather an Alien has hit the Edge next

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,6 +33,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
             # Make the most recently drawn screen visible.
@@ -81,6 +82,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Update the positions of all the aliens in the fleet."""
+        self.aliens.update()
 
     def _create_fleet(self):
         """Create the fleet of Aliens."""
